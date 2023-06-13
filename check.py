ce8a3c9386f51d3fb26ca29a883fbbdd600fa02f
Gustavo Marquez <gustavomarquez@refera.com.br>
2023-06-13 14:02:29 -0300

    basic files

---
+++ check.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+def check_pr_description():
+    pull_request_event = os.environ.get('GITHUB_EVENT_PATH')
+    with open(pull_request_event) as event_file:
+        pr_data = json.load(event_file)
+        description = pr_data['pull_request']['body']
+        description_length = len(description)
+        
+        if description_length < 200 or description_length > 250:
+            print("Invalid pull request description!")
+            print("Description must be between 200 and 250 characters.")
+            sys.exit(1)
+
+check_pr_description()
+
