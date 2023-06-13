81e94716cafcd597a38dae8b450a187164320ff0
Gustavo Marquez <gustavomarquez@refera.com.br>
2023-06-13 14:32:52 -0300

    yml corrected

--- check.py
+++ check.py
@@ -1,11 +1,18 @@
 import os
-import requests
+import json
+import sys
 
 def check_pr_description():
     pull_request_event = os.environ.get('GITHUB_EVENT_PATH')
     with open(pull_request_event) as event_file:
         pr_data = json.load(event_file)
         description = pr_data['pull_request']['body']
+        
+        if description is None:
+            print("Invalid pull request description!")
+            print("Description is empty.")
+            sys.exit(1)
+        
         description_length = len(description)
         
         if description_length < 200 or description_length > 250:
